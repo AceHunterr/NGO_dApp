340584c645199a94ffbda00bc744ff7493dc9be1
AceHunterr <aggarwalmehul26@gmail.com>
2023-05-07 18:54:29 +0530

    Added ethers

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const GOERLI_URL = process.env.GOERLI_URL; 
+// const GOERLI_URL = process.env.GOERLI_URL; 
 const PRIVATE_KEY = process.env.PRIVATE_KEY; 
 module.exports = {
   solidity: "0.8.17", 
@@ -17,4 +17,4 @@ module.exports = {
 }; 
 
 
-7f2ba84579e13e963d1e2f4f1845458a5a2f8baf5c678ba6207414d736b06aa4
+// 7f2ba84579e13e963d1e2f4f1845458a5a2f8baf5c678ba6207414d736b06aa4
